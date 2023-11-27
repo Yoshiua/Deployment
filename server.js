3491771a8a8250932fbf15ca0761de73494c7947
Josh Olson <YoshiOlson@yahoo.com>
2023-11-27 14:47:40 -0700

    Rollbar2

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(express.static(`${__dirname}/public`))
 // include and initialize the rollbar library with your access token
 var Rollbar = require('rollbar')
 var rollbar = new Rollbar({
-  accessToken: '524ccf4cbb514473bdb14a82442d3cca',
+  accessToken: 'e9370fadac8f4761b68ae3eed8222f1f',
   captureUncaught: true,
   captureUnhandledRejections: true,
 })
